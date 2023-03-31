595574e3fa4c1f4c8ef047a4ec9fdaa28219c5ab
lee <lee_malachi@163.com>
2023-03-31 16:39:03 +0800

    允许跨域

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+// 跨域
+const cors = require('cors');
 // 
 const dotenv = require('dotenv');
 dotenv.config()
@@ -16,6 +18,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
